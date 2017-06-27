ef1644f1a80232d6ca32662ca246001f9c7479e4
gen BTC <genBTC@gmx.com>
2017-06-27 01:35:20 -0400

    try fix errors

--- Critical20mAudioAlert.js
+++ Critical20mAudioAlert.js
@@ -4,7 +4,7 @@
 // @author genBTC
 // @namespace https://github.com/genbtc/nagiosScripts/
 // @description This script will string and time match stuff and play audio files as alerts
-// @include https://10.0.0.3/your/Nagios/Page/AggregatePage/HasToBeThatPage/*
+// @match https://10.0.0.3/your/Nagios/Page/AggregatePage/HasToBeThatPage/index.php*
 // @grant none
 // @require https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js
 // ==/UserScript==
@@ -21,7 +21,6 @@ player.preload = 'auto';
 setTimeout(delayStart, startupDelay);
 //set up the loop
 function delayStart() {
-    initialize();
     setInterval(mainLoop, runInterval);
 }
 
@@ -39,16 +38,8 @@ function mainLoop() {
     //Loop through all Critical lines one at a time:
     for (var i=0;i<criticalLines.length;i++) {
         var columns = criticalLines[i];
-        var time = columns[5].split(/[ ,]+/);    //split on whitespace.
-        //create a moment.js duration object that is initialized with all the right fields (the slice is to cut out the last letter.)
-        var minutes = moment.duration({
-                        seconds: time[3].slice(0, -1),
-                        minutes: time[2].slice(0, -1),
-                        hours: time[1].slice(0, -1),
-                        days: time[0].slice(0, -1)
-                    }).asMinutes();
-        //if the minutes arent at 20 yet, skip.
-        if (minutes < 20) return;
+        var time = columns[5].innerHTML;
+        if (!time.includes("0d 2"))
             // double check that the line has the word CRITICAL in column [4].
             // triple check to make sure we didnt already just alert on this, by checking the field we wrote.        
         if (columns[4].innerHTML.includes("CRITICAL") && !columns[3].innerHTML.includes("ALERTED!")) { 
